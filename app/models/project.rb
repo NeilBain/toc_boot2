f0ceacb95b95db0c5e43169704242a31e307b82a
NeilBain <git@realrails.co.uk>
2017-05-23 15:45:43 +0100

    Fixes #2. Summaries now calculating.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy 
   validates :title, presence: true,
-                    length: { minimum: 5 }               
+                    length: { minimum: 5 }   
+                    
+  before_save :reset_totals
+  
+  def reset_totals
+    self.min_duration = tasks.sum(:av_duration)
+    self.max_duration = tasks.sum(:max_duration)
+    self.predicted_duration = tasks.sum(:av_duration)+(tasks.sum(:toc_factor)**0.5*2)
+  end                              
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,17 @@ class Task < ApplicationRecord
   validates :av_duration, numericality: {less_than_or_equal_to: :max_duration}, allow_nil: true 
   validates :max_duration, numericality: {greater_than_or_equal_to: :av_duration}, allow_nil: true
   
-  after_validation :log_errors, :if => Proc.new {|m| m.errors}
-
-  def log_errors
-    Rails.logger.debug self.errors.full_messages.join("\n")
+  before_save :calculate_toc_factor
+  after_save :calculate_project_buffer
+  after_destroy :calculate_project_buffer
+  
+  
+  def calculate_toc_factor
+    self.toc_factor = ((self.max_duration - self.av_duration)/2)**2
+  end
+  
+  def calculate_project_buffer
+    @project = Project.find(self.project_id)
+    @project.update_attributes(min_duration: 1)
   end
 end
